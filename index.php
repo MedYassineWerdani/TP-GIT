5e2f581839dd9173699c9f8bc5e40e9f26ba7adf
MedYassineElwerdani <Mohamedyassine.elwerdani@polytechnicien.tn>
2023-11-16 15:05:36 +0100

    add all changes

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
+<?php
+include('traitement.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="./css/style.css" rel="stylesheet">
+    <script src="./JS/code.js"></script>
 </head> 
 <body>
 
-    <table>
+    <table border="1" cellspacing="0">
         <tr>
             <th>
-                fizz
+                I love my work
             </th>
         </tr>
         <tr>
-            <td>
-                Mid
+            <div class="containter">
+            <td >
+                AI Engineering
             </td>
+        </div>
         </tr>
     </table>
     
